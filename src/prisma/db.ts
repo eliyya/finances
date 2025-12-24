663bf31cc9a64087f3a02ab64f9f2c6807170ae5
Elí Maciel <tecno_eli@hotmail.com>
2025-12-24 01:52:31 -0600

    envolve load env in try catch

--- src/prisma/db.ts
+++ src/prisma/db.ts
@@ -1,21 +1,23 @@
-process.loadEnvFile();
-import { PrismaClient } from "./generated/client.ts";
-import { PrismaPg } from "@prisma/adapter-pg";
+try {
+    process.loadEnvFile()
+} catch {}
+import { PrismaClient } from './generated/client.ts'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 function createPrismaClient() {
-  const adapter =
-    globalThis.adapterGlobal ??
-    new PrismaPg({ connectionString: process.env.DATABASE_URL });
-  return new PrismaClient({ adapter });
+    const adapter =
+        globalThis.adapterGlobal ??
+        new PrismaPg({ connectionString: process.env.DATABASE_URL })
+    return new PrismaClient({ adapter })
 }
 
 declare const globalThis: {
-  dbGlobal: ReturnType<typeof createPrismaClient>;
-  adapterGlobal: PrismaPg;
-} & typeof global;
+    dbGlobal: ReturnType<typeof createPrismaClient>
+    adapterGlobal: PrismaPg
+} & typeof global
 
-const db = globalThis.dbGlobal ?? createPrismaClient();
+const db = globalThis.dbGlobal ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalThis.dbGlobal = db;
+if (process.env.NODE_ENV !== 'production') globalThis.dbGlobal = db
 
-export { db };
+export { db }
